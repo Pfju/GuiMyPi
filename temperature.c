ad715780ebcfc2222eb3cacfbcf4115d8096df82
Pfju <marcelstzb@gmx.de>
2014-09-06 00:47:08 +0200

    refactoring
    replaced fopen by open to hopefully decrease the time reading from
    file...

--- temperature.c
+++ temperature.c
@@ -1,38 +1,39 @@
-
+#include "temperature.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "temperature.h"
+#include <fcntl.h>
+
+#define MAX_READ 500
 
 double GetTemperature(void)
 {
 	double temperature = 0;
-	char arbuffer[500];
+	char arbuffer[MAX_READ];
 	char artemperature[10];
-	int c, k;
-	int i=0;
-	FILE *fp;
-	fp = fopen("/sys/bus/w1/devices/28-000005fd876a/w1_slave", "r");
-	if( fp == NULL )
+	int iNumberofChars;
+	int file_desc, close_err;
+	int k, i;
+
+	file_desc = open("temperature.dat", O_RDONLY);
+	if(file_desc < 0)
 	{
 		printf("Cannot open file!\n");
 		return 1;
 	}
 
 	/* reading entire file into arbuffer */
-	while( (c=fgetc(fp)) != EOF )
-	{
-		arbuffer[i++] = c;
-	}
-	fclose(fp);
+	iNumberofChars = read(file_desc, arbuffer, MAX_READ);
+	/* close file descriptor again */
+	close_err = close(file_desc);
+	if(close_err < 0)
+		printf("Cannot close file!\n");
 
-	/* only reading the artemperature from arbuffer */
-	for(i=69,k=0; i<=73; i++,k++)
+	/* only reading the temperature from buffer */
+	for(i = 69, k = 0; i <= 73; i++, k++)
 	{
 		artemperature[k] = arbuffer[i];
 	}
 	artemperature[k] = '\0';
 	temperature = atof(artemperature);
-
-return (temperature/= 1000);
+	return (temperature/= 1000);
 };
-
